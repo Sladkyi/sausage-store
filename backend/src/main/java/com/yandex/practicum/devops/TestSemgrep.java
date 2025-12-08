b9f7b903af44de646a0e3444dbf81455d3e6e816
Никита <eurczam4@gmail.com>
2025-12-08 13:12:35 +0300

    Test Semgrep integration

---
+++ backend/src/main/java/com/yandex/practicum/devops/TestSemgrep.java
@@ -0,0 +1,9 @@
+// backend/src/main/java/com/yandex/practicum/devops/TestSemgrep.java
+package com.yandex.practicum.devops;
+
+public class TestSemgrep {
+    public static void main(String[] args) {
+        String password = "123456"; // Semgrep должен поймать секрет
+        System.out.println(password);
+    }
+}
